5896af2cc8006875bc97ec6e2e789f97c19ea9ef
git <padung1999@gmail.com>
2022-08-02 11:23:26 +0700

    localStorage

--- todo_app/src/layouts/Main/index.jsx
+++ todo_app/src/layouts/Main/index.jsx
@@ -200,7 +200,7 @@
 
 // -------------------------------------------------------------
 
-import { useState } from "react";
+import { useState/* , useEffect  */} from "react";
 
 import { Routes, Route } from "react-router-dom";
 import TaskList from "../../components/TaskList";
@@ -208,98 +208,103 @@ import AddTask from "../../components/AddTask";
 
 import "./main.scss";
 
-const todoArray = [
-  {
-    id: 1,
-    title: "Task 1",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task",
-  },
-  {
-    id: 2,
-    title: "Task 2",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task.  ",
-  },
-  {
-    id: 3,
-    title: "Task 3",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task.",
-  },
-  {
-    id: 4,
-    title: "Task 4",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task. ",
-  },
-  {
-    id: 5,
-    title: "Task 5",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task. ",
-  },
-  {
-    id: 6,
-    title: "Task 6",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task. ",
-  },
-  {
-    id: 7,
-    title: "Task 7",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task.  ",
-  },
-  {
-    id: 8,
-    title: "Task 8",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task. ",
-  },
-  {
-    id: 9,
-    title: "Task 9",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task. ",
-  },
-  {
-    id: 10,
-    title: "Task 10",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task. ",
-  },
-  {
-    id: 11,
-    title: "Task 11",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task. ",
-  },
-  {
-    id: 12,
-    title: "Task 12",
-    creator: "creator",
-    status: "New",
-    desscription: "This is a task. ",
-  },
-];
+// const todoArray = [
+//   {
+//     id: 1,
+//     title: "Task 1",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task",
+//   },
+//   {
+//     id: 2,
+//     title: "Task 2",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task.  ",
+//   },
+//   {
+//     id: 3,
+//     title: "Task 3",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task.",
+//   },
+//   {
+//     id: 4,
+//     title: "Task 4",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task. ",
+//   },
+//   {
+//     id: 5,
+//     title: "Task 5",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task. ",
+//   },
+//   {
+//     id: 6,
+//     title: "Task 6",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task. ",
+//   },
+//   {
+//     id: 7,
+//     title: "Task 7",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task.  ",
+//   },
+//   {
+//     id: 8,
+//     title: "Task 8",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task. ",
+//   },
+//   {
+//     id: 9,
+//     title: "Task 9",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task. ",
+//   },
+//   {
+//     id: 10,
+//     title: "Task 10",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task. ",
+//   },
+//   {
+//     id: 11,
+//     title: "Task 11",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task. ",
+//   },
+//   {
+//     id: 12,
+//     title: "Task 12",
+//     creator: "creator",
+//     status: "New",
+//     desscription: "This is a task. ",
+//   },
+// ];
 
 function Main() {
+  // const [todoArray, setTodoArray] = useState(JSON.parse(localStorage.getItem('datas')) || []);
+  const [listTodo, setListTodo] = useState(JSON.parse(localStorage.getItem('datas')) || []);
 
-// const todoArray = JSON.parse{ localStorage.}
+  // useEffect(() => {
+  //   if (todoArray) {
+  //    setTodoArray(todoArray);
+  //   }
+  // }, [todoArray]);
 
-  const [listTodo, setListTodo] = useState(todoArray);
 
   const addNewTodo = (valueItem) => {
     setListTodo([
@@ -313,6 +318,8 @@ function Main() {
     ]);
   };
 
+  
+
   const changeStatus = (/* id */ indexItem) => {
 
     // console.log('id: ', id);
@@ -321,18 +328,29 @@ function Main() {
 
     // let indexItem = arrTodo.findIndex(item => item.id === id);
 
-    if(indexItem !== -1){
-      arrTodo.splice(indexItem, 1, {
-        title: listTodo[indexItem].title,
-        creator: listTodo[indexItem].creator,
-        desscription: listTodo[indexItem].desscription,
-        status: listTodo[indexItem].status === "New" ? "Doing" : "Done",
-      });
-      setListTodo([...arrTodo]);
-    }
+    // if(indexItem !== -1){
+    //   arrTodo.splice(indexItem, 1, {
+    //     title: listTodo[indexItem].title,
+    //     creator: listTodo[indexItem].creator,
+    //     desscription: listTodo[indexItem].desscription,
+    //     status: listTodo[indexItem].status === "New" ? "Doing" : "Done",
+    //   });
+    //   setListTodo([...arrTodo]);
+    // }
 
+    arrTodo.splice(indexItem, 1, {
+            title: listTodo[indexItem].title,
+            creator: listTodo[indexItem].creator,
+            desscription: listTodo[indexItem].desscription,
+            status: listTodo[indexItem].status === "New" ? "Doing" : "Done",
+          });
+          setListTodo([...arrTodo]);
    
   };
+
+
+  localStorage.setItem('datas', JSON.stringify(listTodo));
+
 
   return (
     <div className="main">
